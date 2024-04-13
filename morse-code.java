e33234516fad8dd07e0edd461609970b84cbe67a
mohansachinb <128957252+mohansachinb@users.noreply.github.com>
2024-04-13 14:52:22 -0700

    add new file:

---
+++ morse-code.java
@@ -0,0 +1,264 @@
+import java.util.Scanner;
+public class morse-cove {    
+public static void main(String[] args) {    
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter your choice");
+    char n = sc.next().charAt(0);
+    //int n = sc.nextInt();
+    switch(n)  
+    {  
+        case 'A':   
+            System.out.println("._");  
+            break; 
+
+        case 'B':   
+            System.out.println("_...");  
+            break; 
+
+        case 'C':   
+            System.out.println("_._.");  
+            break;
+
+        case 'D':   
+            System.out.println("_..");  
+            break;
+
+        case 'E':   
+            System.out.println(".");  
+            break;
+
+        case 'F':   
+            System.out.println(".._.");  
+            break;
+
+        case 'G':   
+            System.out.println("__.");  
+            break;
+
+        case 'H':   
+            System.out.println("....");  
+            break;
+
+        case 'I':   
+            System.out.println("..");  
+            break;  
+        case 'J':   
+            System.out.println(".___");  
+            break;  
+
+            case 'K':   
+            System.out.println("_._");  
+            break; 
+
+        case 'L':   
+            System.out.println("._..");  
+            break; 
+
+        case 'M':   
+            System.out.println("__");  
+            break; 
+
+        case 'N':   
+            System.out.println("_.");  
+            break;
+
+        case 'O':   
+            System.out.println("___");  
+            break;
+
+        case 'P':   
+            System.out.println(".__.");  
+            break;
+
+        case 'Q':   
+            System.out.println("__._");  
+            break; 
+
+        case 'R':   
+            System.out.println("._.");  
+            break;  
+
+        case 'S':   
+            System.out.println("...");  
+            break;  
+
+        case 'T':   
+            System.out.println("_");  
+            break;  
+
+            case 'U':   
+            System.out.println(".._");  
+            break; 
+
+        case 'V':   
+            System.out.println("..._");  
+            break; 
+
+        case 'W':   
+            System.out.println(".__");  
+            break; 
+
+        case 'X':   
+            System.out.println("_.._");  
+            break;
+
+        case 'Y':   
+            System.out.println("_.__");  
+            break;  
+
+        case 'Z':   
+            System.out.println("__..");  
+            break;
+
+        case 'a':   
+            System.out.println("._");  
+            break; 
+
+        case 'b':   
+            System.out.println("_...");  
+            break; 
+
+        case 'c':   
+            System.out.println("_._.");  
+            break;
+
+        case 'd':   
+            System.out.println("_..");  
+            break; 
+
+        case 'e':   
+            System.out.println(".");  
+            break; 
+
+        case 'f':   
+            System.out.println(".._.");  
+            break; 
+
+        case 'g':   
+            System.out.println("__.");  
+            break;
+
+        case 'h':   
+            System.out.println("....");  
+            break; 
+
+            case 'i':   
+            System.out.println("..");  
+            break; 
+
+        case 'j':   
+            System.out.println(".___");  
+            break; 
+
+        case 'k':   
+            System.out.println("_._");  
+            break;
+
+        case 'l':   
+            System.out.println("._..");  
+            break; 
+
+            case 'm':   
+            System.out.println("__");  
+            break;
+
+        case 'n':   
+            System.out.println("_.");  
+            break; 
+
+        case 'o':   
+            System.out.println("___");  
+            break; 
+
+        case 'p':   
+            System.out.println(".__.");  
+            break;
+
+        case 'q':   
+            System.out.println("__._");  
+            break;
+
+            case 'r':   
+            System.out.println("._.");  
+            break; 
+
+        case 's':   
+            System.out.println("...");  
+            break;
+
+        case 't':   
+            System.out.println("_");  
+            break;
+
+        case 'u':   
+            System.out.println(".._");  
+            break;
+
+        case 'v':   
+            System.out.println("..._");  
+            break;
+
+        case 'w':   
+            System.out.println(".__");  
+            break;
+
+        case 'x':   
+            System.out.println("_.._");  
+            break; 
+
+        case 'y':   
+            System.out.println("_.__");  
+            break; 
+
+        case 'z':   
+            System.out.println("__..");  
+            break; 
+
+
+
+            case '1':   
+            System.out.println(".____");  
+            break; 
+
+        case '2':   
+            System.out.println("..___");  
+            break;
+
+        case '3':   
+            System.out.println("...__");  
+            break;
+
+        case '4':   
+            System.out.println("...._");  
+            break;
+
+        case '5':   
+            System.out.println(".....");  
+            break;
+
+        case '6':   
+            System.out.println("_....");  
+            break;
+
+        case '7':   
+            System.out.println("__...");  
+            break; 
+
+        case '8':   
+            System.out.println("___..");  
+            break; 
+
+        case '9':   
+            System.out.println("____.");  
+            break; 
+
+        case '0':   
+            System.out.println("_____");  
+            break; 
+
+
+        default:   
+            System.out.println("Wrong choice: ");  
+    }  
+}    
+}   
